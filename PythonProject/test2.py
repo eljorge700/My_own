906848830b40125c60ea409e672e55eb5cab284f
eljorge700 <evojorge88@gmail.com>
2025-01-11 15:24:21 +0300

    11/01 test

---
+++ PythonProject/test2.py
@@ -0,0 +1,13 @@
+# string = ('String with some symbols "s"')
+#
+# print(help(str))
+#
+# # result = string.upper()
+# # result2 = string.lower()
+# # result3 = string.count('s')
+# # result4 = string.replace('s', 'S')
+# # print(result)
+# # print(result2)
+# # print(result3)
+# # print(result4)
+
---
+++ PythonProject/test6.py
@@ -0,0 +1,6 @@
+# phone_book = {'George': 8800555500, 'Nadya': 8800555501}
+# phone_book['Boris'] = 8800555502
+# phone_book['Katya'] = 8800555503
+# phone_book.update({'Igor': 8800555504,
+#                   'Nastya': 8800555505})
+# print(phone_book.items())
